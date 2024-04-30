fc80df2699b611e3c1245c930af062899fa94798
Karthik Nair <realkarthiknair@gmail.com>
2024-04-30 16:43:30 +0530

    fetch ratings

--- coursera-scraper/coursera_scraper.py
+++ coursera-scraper/coursera_scraper.py
@@ -15,17 +15,55 @@ def scrap_course(course_link, n):
             "p", class_="cds-ProductCard-partnerNames css-vac8rf"
         )
         partner_names = [partner.text.strip() for partner in partner_elements][:n]
+        stars = soup.find_all("p", class_="css-2xargn")
+        print(f"Found {len(stars)} ratings.")
+        ratings = [star.text for star in stars][:n]
+        print(ratings)
+        reviews_container = soup.find("div", class_="product-reviews css-pn23ng")
+        reviews = (
+            reviews_container.find_all("p", class_="css-vac8rf")
+            if reviews_container
+            else []
+        )
+        review_count = [review.text.strip() for review in reviews]
         script_tag = soup.find("script", type="application/ld+json")
         if script_tag:
             json_data = json.loads(script_tag.string)
             courses = []
             i = 0
             for item in json_data.get("itemListElement", [])[:n]:
+                print("hello")
                 position = item.get("position")
                 url = item.get("url")
                 if url:
-                    courses.append([course_names[i], partner_names[i], url])
+                    try:
+                        course_name = course_names[i]
+                    except IndexError:
+                        course_name = "N/A"
+                    try:
+                        partner_name = partner_names[i]
+                    except IndexError:
+                        partner_name = "N/A"
+                    try:
+                        rating = ratings[i]
+                    except IndexError:
+                        rating = "N/A"
+                    try:
+                        review = review_count[i]
+                    except IndexError:
+                        review = "N/A"
+
+                    course = {
+                        "position": position,
+                        "url": url,
+                        "course_name": course_name,
+                        "partner_name": partner_name,
+                        "rating": rating,
+                        "review": review,
+                    }
+                    courses.append(course)
                     i += 1
+                    print(i)
             return courses
         else:
             print("Error: Unable to find JSON data in the script tag.")
@@ -56,18 +94,22 @@ if __name__ == "__main__":
     link = generate_link(course_name, difficulty)
     print(f"Scraping data from {link}")
     n = 10
-    while True:
-        try:
-            courses = scrap_course(link, n)
-            break
-        except Exception as e:
-            print(f"Error: {e}")
-            if n == 1:
-                break
-            n -= 1
-    if courses:
-        with open(file_name, "w") as f:
-            json.dump(courses, f, indent=2)
-        print(f"Data saved to {file_name}")
+
+    # try:
+    #     courses = scrap_course(link, n)
+    #     break
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    #     if n == 1:
+    #         break
+    #     n -= 1
+    try:
+        courses = scrap_course(link, n)
+        if courses:
+            with open(file_name, "w") as f:
+                json.dump(courses, f, indent=2)
+                print(f"Data saved to {file_name}")
+    except Exception as e:
+        print(f"Error: {e}")
     else:
         print("No data was scraped.")
